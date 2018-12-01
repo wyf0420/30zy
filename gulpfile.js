66d535f23aac6a8116adcc366a8cfa0eabfe7116
wyf <3455667887@qq.com>
2018-12-01 09:31:35 +0800

    头部完成

--- gulpfile.js
+++ gulpfile.js
@@ -8,4 +8,16 @@ var htmlmin = require('gulp-htmlmin');
 var url = require('url');
 var fs = require('fs');
 var path = require('path');
-var babel = require('gulp-babel');
+// var babel = require('gulp-babel');
+
+gulp.task('devScss', function() {
+    return gulp.src('./src/scss/style.scss')
+        .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+        .pipe(scss())
+        .pipe(gulp.dest('./src/css'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./src/scss/style.scss', gulp.series('devScss'))
+})
+gulp.task('dev', gulp.parallel('devScss', 'watch'));
